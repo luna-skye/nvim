6a5bc84c8c5da34317806471c59424f0b1159dac
Luna Skye <sepshuncontact@gmail.com>
2025-04-01 13:42:02 -0400

    feat(plugins): add utility-esque plugins

--- nix/plugins/default.nix
+++ nix/plugins/default.nix
@@ -17,6 +17,19 @@ let
     ./wilder.nix
   ];
 
-in pkgs.lib.mkMerge (builtins.map
-  (v: import v { inherit pkgs helpers; })
-enabledPlugins)
+in pkgs.lib.mkMerge [
+  (pkgs.lib.mkMerge (builtins.map
+    (v: import v { inherit pkgs helpers; })
+  enabledPlugins))
+
+  { # misc. configuration
+    plugins.nvim-autopairs = { enable = true; };
+    plugins.nvim-surround = { enable = true; };
+    plugins.sleuth = { enable = true; };
+    plugins.multicursors = { enable = true; };
+
+    keymaps = [
+      (helpers.mkKeymap [ "n" "v" ] "<leader>ms" "<CMD>MCstart<CR>" "Start multicursor selection")
+    ];
+  }
+]
