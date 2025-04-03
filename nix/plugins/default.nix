94d9a0c6e2377d251f4d53455676a3ac0f31255d
Luna Skye <sepshuncontact@gmail.com>
2025-04-03 08:56:11 -0400

    style(plugins): simplify plugin module loading

--- nix/plugins/default.nix
+++ nix/plugins/default.nix
@@ -1,36 +1,17 @@
-{ pkgs, helpers, ... }: 
-
-let
-  enabledPlugins = [
-    ./ccc.nix
-    ./cmp.nix
-    ./git.nix
-    ./lsp.nix
-    ./lualine.nix
-    ./oil.nix
-    ./snacks.nix
-    ./statuscol.nix
-    ./tmux.nix
-    ./treesitter.nix
-    ./trouble.nix
-    ./web-devicons.nix
-    ./whichkey.nix
-    ./wilder.nix
-  ];
-
-in pkgs.lib.mkMerge [
-  (pkgs.lib.mkMerge (builtins.map
-    (v: import v { inherit pkgs helpers; })
-  enabledPlugins))
-
-  { # misc. configuration
-    plugins.nvim-autopairs = { enable = true; };
-    plugins.nvim-surround = { enable = true; };
-    plugins.sleuth = { enable = true; };
-    plugins.multicursors = { enable = true; };
-
-    keymaps = [
-      (helpers.mkKeymap [ "n" "v" ] "<leader>ms" "<CMD>MCstart<CR>" "Start multicursor selection")
-    ];
-  }
-]
+{ pkgs, helpers, ... }:
+pkgs.lib.mkMerge (builtins.map (v: import v { inherit pkgs helpers; }) [
+  ./ccc.nix
+  ./cmp.nix
+  ./git.nix
+  ./lsp.nix
+  ./lualine.nix
+  ./oil.nix
+  ./snacks.nix
+  ./statuscol.nix
+  ./tmux.nix
+  ./treesitter.nix
+  ./trouble.nix
+  ./web-devicons.nix
+  ./whichkey.nix
+  ./wilder.nix
+])
