ad99e456ffadfad975f975cc07560dc9c11ab1d2
Luna Skye <sepshuncontact@gmail.com>
2025-04-03 09:36:14 -0400

    fix(plugins/statuscol): gitsigns not showing

--- nix/plugins/statuscol.nix
+++ nix/plugins/statuscol.nix
@@ -5,25 +5,22 @@
       clickmod = "c";
       ft_ignore = null;
       relculright = true;
+
       segments = let
         empty = { text = [ " " ]; };
       in [
         (empty)
-        { click = "v:lua.ScFa"; text = [ "%C" ]; }
-        # { click = "v:lua.ScSa"; text = [ "%s" ]; }
-        {
-          sign = { namespace = [ "gitsigns" ]; colwidth = 1; wrap = true; };
-          click = "v:lua.ScSa";
-        }
+        { click = "v:lua.ScFa"; text = [ " " "%C" ]; }
+        { click = "v:lua.ScSa"; text = [ " " "%s" ]; }
         (empty)
         {
           click = "v:lua.ScLa";
-          condition = [
-            true { __raw = "require('statuscol.builtin').not_empty"; }
-          ];
-          text = [
-            { __raw = "require('statuscol.builtin').lnumfunc"; } " "
-          ];
+          condition = [ true { __raw = "require('statuscol.builtin').not_empty"; } ];
+          text = [ { __raw = "require('statuscol.builtin').lnumfunc"; } " " ];
+        }
+        {
+          sign = { namespace = [ "gitsigns" ]; colwidth = 1; wrap = true; };
+          click = "v:lua.ScSa";
         }
         { text = [ " " ]; condition = [ true { __raw = "require('statuscol.builtin').not_empty"; } ]; }
       ];
